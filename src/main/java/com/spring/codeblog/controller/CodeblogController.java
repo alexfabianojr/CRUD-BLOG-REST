685d508bffe0e6e89e1c175ef853aa3181c44435
Alex Fabiano Baptista da Silva Junior <alexfabianojr@gmail.com>
2020-07-03 03:52:53 -0300

    controller

---
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -0,0 +1,27 @@
+package com.spring.codeblog.controller;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CodeblogController {
+
+    @Autowired
+    CodeblogService codeblogService;
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
+        ModelAndView modelAndView = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        modelAndView.addObject("posts", posts);
+        return modelAndView;
+    }
+}
